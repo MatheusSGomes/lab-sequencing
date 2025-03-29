a8bf9883b450459332205c03ab9baa7ccf395c4f
Matheus Gomes <math.s.gomes@gmail.com>
2025-03-28 23:11:45 -0300

    add imports pg and dotenv config

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-const express = require('express')
+const express = require('express');
+require("dotenv").config();
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+console.log(process.env.CONNECTION_STRING);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+// bootstrap
+// const sql = `
+//   CREATE TABLE rastreamento (
+//       id SERIAL PRIMARY KEY, -- Também poderia usar DEFAULT nextval('tracking_id_seq')
+//       tracking_id INT NOT NULL DEFAULT nextval('tracking_id_seq'),
+//       pedido_id INT NULL,
+//       transportadora_id INT NULL,
+//       status VARCHAR(50) NOT NULL DEFAULT 'Em trânsito',
+//       data_envio TIMESTAMP DEFAULT NOW()
+//   );
+// `;
+
+// await db.any(sql);
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
+
+// {
+//   "dependencies": {
+//     "dotenv": "^16.4.7",
+//     "express": "^4.21.2",
+//     "pg": "^8.14.1"
+//   }
+// }
+
