36d84b4e5f66e33140c35406961aedd128e3c51b
Matheus Gomes <math.s.gomes@gmail.com>
2025-03-31 22:07:25 -0300

    fix get all trackings

--- repository.js
+++ repository.js
@@ -3,6 +3,5 @@ import { connect } from "./connection.js";
 export async function getAllTrackings() {
     const client = await connect();
     const res = await client.query('SELECT * FROM tracking;')
-    console.log(res);
     return await res.rows;
 }
