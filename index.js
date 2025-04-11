bae2d804c1a343e3b351e50dd7541f9a2bd505e0
Matheus Gomes <math.s.gomes@gmail.com>
2025-04-11 20:58:38 -0300

    move routes folder to src

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import 'dotenv/config'
 import { startConnectionDatabase } from './connection.js';
-import trackingRouter from './routes/routes/tracking.routes.js';
-import orderRouter from './routes/routes/order.routes.js';
+import trackingRouter from './src/routes/tracking.routes.js';
+import orderRouter from './src/routes/order.routes.js';
 import routerGenerateTrackings from './src/cron/generate-trackings.js';
 
 const app = express()
