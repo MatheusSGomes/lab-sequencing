629b789f739ec3024f77e5dacc1dc8ecd85ebe40
Matheus Gomes <math.s.gomes@gmail.com>
2025-04-11 21:02:02 -0300

    add port to variable

--- index.js
+++ index.js
@@ -11,9 +11,10 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(process.env.PORT, () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
     startConnectionDatabase();
-    console.log(`Listening on port ${process.env.PORT}`);
+    console.log(`Listening on port ${PORT}`);
 });
 
 app.get('/health', (req, res) => res.send('OK'));
