2d6c3610fa9c724efed717f9b434479a5ab1a52f
Matheus Gomes <math.s.gomes@gmail.com>
2025-04-11 21:03:59 -0300

    change index to app.js

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import 'dotenv/config'
+import { startConnectionDatabase } from '../connection.js';
+import trackingRouter from './routes/tracking.routes.js';
+import orderRouter from './routes/order.routes.js';
+import routerGenerateTrackings from './cron/generate-trackings.js';
+
+const app = express()
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/tracking', trackingRouter);
+app.use('/order', orderRouter);
+app.use('', routerGenerateTrackings);
+
+app.get('/health', (req, res) => res.send('OK'));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    startConnectionDatabase();
+    console.log(`Listening on port ${PORT}`);
+});
